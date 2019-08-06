7494f743fd17e293b1541c7be563d5162860f455
Liujinghhh <1944465209@qq.com>
2019-08-06 15:09:28 +0800

    feat 滚动加载

--- src/views/HomeHot.vue
+++ src/views/HomeHot.vue
@@ -21,11 +21,32 @@ export default {
     HomeHotTag,
   },
   mounted () {
-    this.$store.dispatch(FETCH_QUESTION_HOT)
+    this.$store.dispatch(FETCH_QUESTION_HOT,this.hotPage)
+    window.addEventListener('scroll',this.handleScroll)
+     
+    
   },
   computed: {
-    ...mapGetters(['questionList']),
+    ...mapGetters(['hotPage','questionList']),
   },
+  methods:{
+   handleScroll(){
+     let scrollTop = document.documentElement.scrollTop
+     let client = document.documentElement.clientHeight
+     let scrollHeight = document.documentElement.scrollHeight
+     
+     if (scrollTop+client >= scrollHeight){
+       this.loadMore()
+      
+
+     }
+
+   },
+   loadMore(){
+   
+    
+   }
+  }
 }
 </script>
 
